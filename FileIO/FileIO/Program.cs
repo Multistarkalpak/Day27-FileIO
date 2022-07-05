30987dd79fb0eb58f632b0a9fc0e8d03a5dce102
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-07-05 22:47:01 +0530

    Checking File exist or not

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace FileIO
+
+{
+    class Program
+    {
+        public static void FileExist()
+        {
+            string InputFile = @"C:\Users\kalpak\Desktop\Project.txt";
+            if (File.Exists(InputFile))
+            {
+                Console.WriteLine("File Exist");
+            }
+            else
+            {
+                Console.WriteLine("Not Exist");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+
+            FileExist();
+
+        }
+    }
+}
